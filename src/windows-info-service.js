601eb693824a0d5caa6b8b9ea7e4798569b81d44
Ewan Harris <ewanharris93@gmail.com>
2018-02-14 15:37:30 +0000

    Changes for windowslib2

--- src/windows-info-service.js
+++ src/windows-info-service.js
@@ -1,10 +1,23 @@
 import DetectEngine from 'appcd-detect';
+import fs from 'fs';
 import gawk from 'gawk';
+import path from 'path';
 import * as windowslib from 'windowslib';
 import * as registry from 'appcd-winreg';
+import version from './version';
 
 import { DataServiceDispatcher } from 'appcd-dispatcher';
-import { get } from 'appcd-util';
+import { expandPath } from 'appcd-path';
+import { cache, debounce as debouncer, get, arrayify } from 'appcd-util';
+
+let vswhere;
+
+/**
+ * Constants to identify the subscription id list.
+ * @type {Number}
+ */
+const VISUAL_STUDIO_INSTALL_DIR = 1;
+const VSWHERE_INSTALL_DIR = 2;
 
 /**
  * The Windows info service.
@@ -20,11 +33,18 @@ export default class WindowsInfoService extends DataServiceDispatcher {
 	 */
 	async activate(cfg) {
 		this.data = gawk({
+			devices: [],
 			sdk: [],
 			visualstudio: {}
 		});
 
 		this.timers = {};
+
+		/**
+		 * A map of buckets to a list of active fs watch subscription ids.
+		 * @type {Object}
+		 */
+		this.subscriptions = {};
 
 		// // wire up Visual Studio detection first so that we can use its result to know if we should query the other thing
 		// await this.wireupDetection('visualstudio', get(cfg, 'windows.visualstudio.pollInterval') || 60000 * 10, () => this.detectVisualStudios());
@@ -38,17 +58,22 @@ export default class WindowsInfoService extends DataServiceDispatcher {
 		// // wire up devices after the rest to avoid DAEMON-173 where emulator and
 		// // device detect functions attempt to build and write wptool at the same time
 		// await this.wireupDetection('devices',      get(cfg, 'windows.device.pollInterval')       || 2500,       () => this.detectDevices());
-
-		await this.initSDK();
-		await this.initVS();
+		await Promise.all([
+			this.initSDKAndDevices(),
+			this.initVS()
+		]);
 	}
 
-	async initSDK() {
+	async initSDKAndDevices() {
 
 		this.sdkDetectEngine = new DetectEngine({
 			checkDir(dir) {
 				try {
-					return new windowslib.SDK(dir);
+					const sdk = new windowslib.SDK(dir);
+					// Lets just ignore everything below 10
+					if (version.gte(sdk.version, '10')) {
+						return sdk;
+					}
 				} catch (e) {
 					// Do nothing
 					console.log(e);
@@ -94,34 +119,127 @@ export default class WindowsInfoService extends DataServiceDispatcher {
 			multiple: true
 		});
 
-		this.sdkDetectEngine.on('results', results => {
+		this.sdkDetectEngine.on('results', async results => {
 			gawk.set(this.data.sdk, results);
 		});
 
-		await this.sdkDetectEngine.start();
+		return this.sdkDetectEngine.start()
+			.then(async results => {
+				if (results && results[0] && results[0].revisions) {
+					const rev = results[0].revisions[0];
+					gawk.set(this.data.devices, await windowslib.device.getDevices({ sdk: rev }));
+					this.rescanDevices();
+				}
+			});
 	}
 
 	async initVS() {
-		let	vswhere = await windowslib.vswhere.getVSWhere();
-		if (vswhere) {
-			try {
-				const { code, stdout, stderr } = await vswhere.detect();
-				if (!code) {
-					const visualStudios = {};
-					const visualStudioData = JSON.parse(stdout);
-					for (const vs of visualStudioData) {
-						try {
-							const visualStudio = new windowslib.VisualStudio(vs);
-							visualStudios[vs.installationVersion] = visualStudio;
-						} catch (e) {
-							console.log(e);
-						}
-					}
-					gawk.set(this.data.visualstudio, visualStudios);
-				}
-			} catch (e) {
-				console.log(e);
+		const defaultDir = expandPath(windowslib.visualstudio.defaultInstallDir);
+		if (fs.existsSync(defaultDir)) {
+			let dirs = fs.readdirSync(defaultDir).filter(dir => parseInt(dir, 10));
+			const paths = [];
+			for (const dir of dirs) {
+				paths.push(path.join(defaultDir, dir.toString()));
 			}
+			// FIXME: Watching recursively seems to be watching way too many dirs when we install
+			this.watch({
+				type: VISUAL_STUDIO_INSTALL_DIR,
+				paths,
+				debounce: true,
+				handler: async (msg) => {
+					if ((msg.action === 'add' || msg.action === 'change') && !this.subscriptions.hasOwnProperty(msg.file)) {
+						this.subscriptions[msg.file] = {};
+						this.watch({
+							type: msg.file,
+							paths: [ msg.file ],
+							debounce: true,
+							handler: async () => {
+								const visualStudios = await this.scanVisualStudio();
+								gawk.set(this.data.visualstudio, visualStudios);
+							}
+						});
+					}
+				},
+				depth: 1
+			});
+		} else {
+			// Otherwise just watch it til it comes around
+			this.watch({
+				type: VISUAL_STUDIO_INSTALL_DIR,
+				paths: [ windowslib.visualstudio.defaultInstallDir ],
+				debounce: true,
+				handler: () => {
+					this.scanVisualStudio();
+				}
+			});
+		}
+
+		this.watch({
+			type: VSWHERE_INSTALL_DIR,
+			paths: [ windowslib.vswhere.defaultPath ],
+			debounce: true,
+			handler: () => {
+				this.scanVisualStudio();
+			}
+		});
+		await this.scanVisualStudio();
+	}
+
+	async scanVisualStudio() {
+		vswhere = await windowslib.vswhere.getVSWhere();
+		if (vswhere) {
+			const { code, stdout, stderr } = await vswhere.detect();
+			const vsIds = new Set(Object.keys(this.data.visualstudio));
+			if (!code) {
+				const visualStudios = {};
+				const visualStudioData = JSON.parse(stdout);
+				for (const vs of visualStudioData) {
+					try {
+						const visualStudio = new windowslib.VisualStudio(vs);
+						visualStudios[vs.instanceId] = visualStudio;
+						vsIds.delete(vs.instanceId);
+						if (!this.subscriptions.hasOwnProperty(vs.instanceId)) {
+							this.subscriptions[vs.instanceId] = {};
+							// Set up watchers for each VS install so we can rescan
+							if (this.subscriptions.hasOwnProperty(vs.installationPath)) {
+								this.unwatch(vs.installationPath);
+							}
+							this.watch({
+								type: vs.instanceId,
+								paths: [ vs.installationPath ],
+								debounce: true,
+								handler: () => {
+									this.scanVisualStudio();
+								}
+							});
+						}
+					} catch (e) {
+						console.log(e);
+					}
+				}
+
+				for (const id of vsIds) {
+					this.unwatch(id);
+				}
+				gawk.set(this.data.visualstudio, visualStudios);
+			}
+		}
+	}
+
+	async rescanDevices() {
+		// TODO: Move to a constant with overridable config values
+		const interval = 2500;
+		if (this.data.sdk && this.data.sdk[0] && this.data.sdk[0].revisions) {
+			const rev = this.data.sdk[0].revisions[0];
+			const devices = await windowslib.device.getDevices({ sdk: rev });
+			console.log(devices);
+			if (devices) {
+				console.log('Updating data for devices');
+				gawk.set(this.data.devices, devices);
+			}
+			this.timers.device = setTimeout(() => {
+				this.rescanDevices();
+			}, interval);
 		}
 	}
 
@@ -138,177 +256,83 @@ export default class WindowsInfoService extends DataServiceDispatcher {
 	}
 
 	/**
-	 * Executes a detect function, then stores the result and schedules the next check.
+	 * Subscribes to filesystem events for the specified paths.
 	 *
-	 * @param {String} type - The bucket name for the detected results.
-	 * @param {Number} interval - The amount of milliseconds until the next check.
-	 * @param {Function} callback - A function to call that performs the detection.
+	 * @param {Object} params - Various parameters.
+	 * @param {Boolean} [params.debounce=false] - When `true`, wraps the `handler` with a debouncer.
+	 * @param {Number} [params.depth] - The max depth to recursively watch.
+	 * @param {Function} params.handler - A callback function to fire when a fs event occurs.
+	 * @param {Array.<String>} params.paths - One or more paths to watch.
+	 * @param {String} params.type - The type of subscription.
+	 * @access private
+	 */
+	watch({ debounce, depth, handler, paths, type }) {
+		const callback = debounce ? debouncer(handler) : handler;
+
+		for (const path of paths) {
+			const data = { path };
+			if (depth) {
+				data.recursive = true;
+				data.depth = depth;
+			}
+
+			appcd
+				.call('/appcd/fswatch', {
+					data,
+					type: 'subscribe'
+				})
+				.then(ctx => {
+					let sid;
+					ctx.response
+						.on('data', async (data) => {
+							if (data.type === 'subscribe') {
+								sid = data.sid;
+								if (!this.subscriptions[type]) {
+									this.subscriptions[type] = {};
+								}
+								this.subscriptions[type][data.sid] = 1;
+							} else if (data.type === 'event') {
+								callback(data.message);
+							}
+						})
+						.on('end', () => {
+							if (sid && this.subscriptions[type]) {
+								delete this.subscriptions[type][sid];
+							}
+						});
+				});
+		}
+	}
+
+	/**
+	 * Unsubscribes a list of filesystem watcher subscription ids.
+	 *
+	 * @param {Number} type - The type of subscription.
+	 * @param {Array.<String>} [sids] - An array of subscription ids to unsubscribe. If not
+	 * specified, defaults to all sids for the specified types.
 	 * @returns {Promise}
 	 * @access private
 	 */
-	wireupDetection(type, interval, callback) {
-		return callback()
-			.then(result => {
-				if (result) {
-					console.log(`Updating data for ${type}`);
-					gawk.set(this.data[type], result);
-				}
-			})
-			.catch(err => {
-				console.log(err);
-			})
-			.then(() => {
-				this.timers[type] = setTimeout(() => {
-					this.wireupDetection(type, interval, callback);
-				}, interval);
+	async unwatch(type, sids) {
+		if (!this.subscriptions[type]) {
+			return;
+		}
+
+		if (!sids) {
+			sids = Object.keys(this.subscriptions[type]);
+		}
+
+		for (const sid of sids) {
+			await appcd.call('/appcd/fswatch', {
+				sid,
+				type: 'unsubscribe'
 			});
-	}
 
-	/**
-	 * Checks if there are any Visual Studios installed.
-	 *
-	 * @returns {Boolean}
-	 * @access private
-	 */
-	haveVisualStudio() {
-		return Object.keys(this.data.visualstudio).length > 0;
-	}
+			delete this.subscriptions[type][sid];
+		}
 
-	/**
-	 * Detect Windows Phone devices.
-	 *
-	 * @returns {Promise<Array.<Object>>}
-	 * @access private
-	 */
-	detectDevices() {
-		return new Promise((resolve, reject) => {
-			if (!this.haveVisualStudio()) {
-				return resolve();
-			}
-
-			console.log('Detecting devices info');
-			windowslib.device.detect({ bypassCache: true }, (err, results) => {
-				if (err) {
-					reject(err);
-				} else {
-					const devices = results.devices;
-					let wpsdkIndex = -1;
-					let realDeviceIndex = -1;
-					for (let i = 0; i < devices.length; i++) {
-						const device = devices[i];
-						if (device.udid === 0 && device.wpsdk) {
-							wpsdkIndex = i;
-						} else if (device.udid !== 0 && !device.wpsdk) {
-							// now find with "real" device
-							realDeviceIndex = i;
-						}
-						if (wpsdkIndex !== -1 && realDeviceIndex !== -1) {
-							break;
-						}
-					}
-					if (wpsdkIndex !== -1 && realDeviceIndex !== -1) {
-						// set 'real' device wpsdk to the value we got from wptool binary
-						devices[realDeviceIndex].wpsdk = devices[wpsdkIndex].wpsdk;
-						// remove the wptool binary entry
-						devices.splice(wpsdkIndex, 1);
-					}
-					resolve(devices);
-				}
-			});
-		});
-	}
-
-	/**
-	 * Detect Windows Phone emulators.
-	 *
-	 * @returns {Promise<Object>}
-	 * @access private
-	 */
-	detectEmulators() {
-		return new Promise((resolve, reject) => {
-			if (!this.haveVisualStudio()) {
-				return resolve();
-			}
-
-			console.log('Detecting emulator info');
-			windowslib.emulator.detect({ bypassCache: true }, (err, results) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(results.emulators);
-				}
-			});
-		});
-	}
-
-	/**
-	 * Detect Visual Studio installations.
-	 *
-	 * @returns {Promise<Object>}
-	 * @access private
-	 */
-	detectVisualStudios() {
-		return new Promise((resolve, reject) => {
-			console.log('Detecting visualstudio info');
-			windowslib.visualstudio.detect({ bypassCache: true }, (err, results) => {
-				if (err) {
-					return reject(err);
-				}
-
-				let found = false;
-				if (results.visualstudio) {
-					for (const visualstudio of Object.keys(results.visualstudio)) {
-						if (results.visualstudio[visualstudio].selected) {
-							found = true;
-							gawk.set(this.data.selectedVisualStudio, results.visualstudio[visualstudio]);
-							break;
-						}
-					}
-				}
-				if (!found) {
-					this.data.selectedVisualStudio = null;
-				}
-
-				resolve(results.visualstudio);
-			});
-		});
-	}
-
-	/**
-	 * Detect Windows Store SDK information.
-	 *
-	 * @returns {Promise<Object>}
-	 * @access private
-	 */
-	detectWindowsSDKs() {
-		return new Promise((resolve, reject) => {
-			console.log('Detecting windows store info');
-			windowslib.winstore.detect({ bypassCache: true }, (err, results) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(results.windows);
-				}
-			});
-		});
-	}
-
-	/**
-	 * Detect Windows Phone SDK information.
-	 *
-	 * @returns {Promise<Object>}
-	 * @access private
-	 */
-	detectWindowsPhone() {
-		return new Promise((resolve, reject) => {
-			console.log('Detecting windowsphone info');
-			windowslib.windowsphone.detect({ bypassCache: true }, (err, results) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(results.windowsphone);
-				}
-			});
-		});
+		if (!Object.keys(this.subscriptions[type]).length) {
+			delete this.subscriptions[type];
+		}
 	}
 }
